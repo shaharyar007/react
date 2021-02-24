7162219c0d8c8bd210584b46eaed7458a994bb0a
shaharyar007 <shaharyar295@gmail.com>
2021-02-23 22:42:06 -0500

    counter complete

--- src/Counter.js
+++ src/Counter.js
@@ -6,10 +6,36 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 export default class Counter extends React.Component{
+  constructor(props){
+    super(props);
+    this.inc =  this.inc.bind(this.inc);
+    this.dec = this.dec.bind(this.dec);
+    this.state = {
+number : 12
+
+    };
+  }
+  inc = () => {
+      this.setState({
+        number : this.state.number + 1
+      });
+  }
+  dec = () =>{
+    this.setState({
+      number : this.state.number - 1
+    });
+  }
+ 
     render(){
     return(
       <div className="CompContainer">
         <h2>Counter component</h2>
+        <div className="counter_container">
+         <button onClick={this.inc}>+</button>
+         <button onClick={this.dec}>-</button>
+         <h1>{this.state.number}</h1>
+
+        </div>
       </div>
     )
   }
